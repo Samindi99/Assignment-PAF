527e4a227a4d39fa16164129d9ac6ebc03b01455
Samindi <samindirathnayake99@gmail.com>
2021-05-13 23:15:48 +0530

    Database Connection

--- PAF-Assignment/src/com/Order.java
+++ PAF-Assignment/src/com/Order.java
@@ -2,5 +2,20 @@ package com;
 import java.sql.*;
 public class Order
 { 
+	private Connection connect() 
+	{ 
+		Connection con = null; 
+		
+		try
+		{ 
+			Class.forName("com.mysql.jdbc.Driver"); 
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3307/paf_assignment", "root", "");
+		} 
+		catch (Exception e) 
+		{ 
+			e.printStackTrace(); 
+		}
+		return con; 
+	} 
 	
 }				 
