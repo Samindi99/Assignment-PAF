d4497e9f2049f981c5266d8a5361ff4e9ecc66c0
Samindi <samindirathnayake99@gmail.com>
2021-05-13 23:18:14 +0530

    Retrieve Order Function

--- PAF-Assignment/src/com/Order.java
+++ PAF-Assignment/src/com/Order.java
@@ -18,4 +18,56 @@ public class Order
 		return con; 
 	} 
 	
+	public String readOrder() 
+	{ 
+		String output = ""; 
+		
+		try
+		{ 
+			Connection con = connect(); 
+ 
+			if (con == null) 
+			{ 
+				return "Error while connecting to the database for reading."; 
+			} 
+ 
+			// Prepare the html table to be displayed
+			output = "<table border='1'><tr><th>Order Description</th> <th>Order Value</th><th>Order Quantity</th> <th>Update</th><th>Remove</th></tr>"; 
+ 
+			String query = "select * from paf_assignment.assignment_order"; 
+			Statement stmt = con.createStatement(); 
+			ResultSet rs = stmt.executeQuery(query); 
+ 
+			// iterate through the rows in the result set
+			while (rs.next()) 
+			{ 
+				String orderID = Integer.toString(rs.getInt("orderID")); 
+				String orderDescription = rs.getString("orderDescription");
+				String orderValue = Double.toString(rs.getDouble("orderValue"));
+				String orderQuantity = rs.getString("orderQuantity");
+				 
+				// Add into the html table
+				output += "<tr><td><input id='hidOrderIDUpdate' name='hidOrderIDUpdate' type='hidden' value='" + orderID
+				 + "'>" + orderDescription + "</td>"; 
+				 output += "<td>" + orderValue + "</td>"; 
+				 output += "<td>" + orderQuantity + "</td>"; 
+				 
+				 // buttons
+				 output += "<td><input name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-secondary' data-orderid='" + orderID +"'></td> "
+				 		+ "<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-orderid='" + orderID + "'></td></tr>";
+			} 
+			
+			con.close(); 
+				 
+			// Complete the html table
+			output += "</table>"; 
+				 
+		} 
+		catch (Exception e) 
+		{ 
+			output = "Error while reading the orders."; 
+			System.err.println(e.getMessage()); 
+		} 
+		return output; 
+	}
 }				 
