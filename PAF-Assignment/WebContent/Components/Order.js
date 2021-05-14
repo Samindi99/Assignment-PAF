4689c46ccbf0f08b79c6c8a89f67ab7bf11784fe
Samindi <samindirathnayake99@gmail.com>
2021-05-14 10:53:10 +0530

    Order Js Save Complete

--- PAF-Assignment/WebContent/Components/Order.js
+++ PAF-Assignment/WebContent/Components/Order.js
@@ -40,3 +40,32 @@ $(document).on("click", "#btnSave", function(event)
  		} 
  	}); 
 }); 
+
+function onOrderSaveComplete(response, status)
+{ 
+	if (status == "success") 
+ 	{ 
+ 		var resultSet = JSON.parse(response); 
+ 
+		if (resultSet.status.trim() == "success") 
+ 		{ 
+			 $("#alertSuccess").text("Successfully saved."); 
+			 $("#alertSuccess").show(); 
+			 $("#divOrdersGrid").html(resultSet.data); 
+ 		} else if (resultSet.status.trim() == "error") 
+ 		{ 
+ 			$("#alertError").text(resultSet.data); 
+ 			$("#alertError").show(); 
+ 		} 
+ 	} else if (status == "error") 
+	{ 
+ 		$("#alertError").text("Error while saving."); 
+ 		$("#alertError").show(); 
+ 	} else
+ 	{ 
+ 		$("#alertError").text("Unknown error while saving.."); 
+		$("#alertError").show(); 
+ 	} 
+	$("#hidOrderIDSave").val(""); 
+ 	$("#formOrder")[0].reset(); 
+}
