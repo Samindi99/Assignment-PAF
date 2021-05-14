b0cd73bc022836aa036795e04d937a76760a98d4
Samindi <samindirathnayake99@gmail.com>
2021-05-14 10:32:15 +0530

    Order Js Save

--- PAF-Assignment/WebContent/Components/Order.js
+++ PAF-Assignment/WebContent/Components/Order.js
@@ -7,3 +7,36 @@ if ($("#alertSuccess").text().trim() == "")
  	$("#alertError").hide(); 
 }); 
 
+// SAVE ============================================
+$(document).on("click", "#btnSave", function(event) 
+{ 
+	// Clear alerts---------------------
+	 $("#alertSuccess").text(""); 
+	 $("#alertSuccess").hide(); 
+	 $("#alertError").text(""); 
+	 $("#alertError").hide(); 
+
+	// Form validation-------------------
+	var status = validateOrderForm(); 
+	if (status != true) 
+ 	{ 
+ 		$("#alertError").text(status); 
+ 		$("#alertError").show(); 
+ 		return; 
+ 	} 
+
+	// If valid------------------------
+ 	var type = ($("#hidOrderIDSave").val() == "") ? "POST" : "PUT"; 
+ 	
+	$.ajax( 
+ 	{ 
+ 		url : "OrderAPI", 
+ 		type : type, 
+ 		data : $("#formOrder").serialize(), 
+ 		dataType : "text", 
+ 		complete : function(response, status) 
+ 		{ 
+ 			onOrderSaveComplete(response.responseText, status); 
+ 		} 
+ 	}); 
+}); 
